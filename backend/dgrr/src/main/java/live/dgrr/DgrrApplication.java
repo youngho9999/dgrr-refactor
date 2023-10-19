2a054b896fae5e1193377369293698ef1897a55e
이수연 <sooyeon990828@naver.com>
2023-10-19 16:12:43 +0900

    chore: initial setting

---
+++ backend/dgrr/src/main/java/live/dgrr/DgrrApplication.java
@@ -0,0 +1,13 @@
+package live.dgrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgrrApplication.class, args);
+	}
+
+}
