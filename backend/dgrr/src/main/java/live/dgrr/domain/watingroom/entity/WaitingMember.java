ae209f9b58f372c68c1ef4d081af0606c82b5e81
이수연 <sooyeon990828@naver.com>
2023-10-23 17:35:56 +0900

    feat: WaitingMember 객체 생성

---
+++ backend/dgrr/src/main/java/live/dgrr/domain/watingroom/entity/WaitingMember.java
@@ -0,0 +1,21 @@
+package live.dgrr.domain.watingroom.entity;
+
+import jakarta.persistence.Id;
+import live.dgrr.global.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+
+@Getter
+@RedisHash(value = "waitingmember")
+@NoArgsConstructor
+@AllArgsConstructor
+public class WaitingMember extends BaseEntity {
+    @Id
+    private String waitingMemberId;
+    private String nickname;
+    private String profileImage;
+
+}
