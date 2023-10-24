961aa69f9b3c8b04c9107f01631959ac3ae3c539
전영호 <aplblypt@gmail.com>
2023-10-24 14:56:04 +0900

    fix: gameSessionId -> gameId

--- backend/dgrr/src/main/java/live/dgrr/domain/openvidu/OpenviduService.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/openvidu/OpenviduService.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface OpenviduService {
-    String createSession(String gameSessionId);
+    String createSession(String gameId);
     String createConnection(String sessionId);
 }
