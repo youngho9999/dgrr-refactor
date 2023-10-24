7ae44f3149008279b8e89edb1fc3f5bd38c3a28d
전영호 <aplblypt@gmail.com>
2023-10-24 16:37:40 +0900

    fix: GameMember 에 memberId 필드추가. @jsonignore 추가

--- backend/dgrr/src/main/java/live/dgrr/domain/game/controller/GameController.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/controller/GameController.java
@@ -1,7 +1,9 @@
 package live.dgrr.domain.game.controller;
 
+import live.dgrr.domain.watingroom.entity.GameStartEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,9 @@ public class GameController {
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
-    @GetMapping("/game")
+    @MessageMapping("/game")
     public void test1() {
-
+        applicationEventPublisher.publishEvent(new GameStartEvent("oneId","twoId"));
     }
 
 }
