e1a25f0d8f5d864daf196d0abac7d2cb153005d9
전영호 <aplblypt@gmail.com>
2023-10-24 16:16:02 +0900

    feat: GameFirstRoundService 생성, 게임 생성 초기로직

---
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/service/GameFirstRoundService.java
@@ -0,0 +1,41 @@
+package live.dgrr.domain.game.service;
+
+import live.dgrr.domain.game.entity.GameMember;
+import live.dgrr.domain.game.entity.GameRoom;
+import live.dgrr.domain.game.entity.GameStartEvent;
+import live.dgrr.domain.game.entity.GameStatus;
+import live.dgrr.domain.game.repository.GameRoomRepository;
+import live.dgrr.domain.member.service.MemberService;
+import live.dgrr.domain.openvidu.OpenviduService;
+import live.dgrr.global.entity.Rank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class GameFirstRoundService {
+
+    private final MemberService memberService;
+    private final OpenviduService openviduService;
+    private final GameRoomRepository gameRoomRepository;
+    @EventListener
+    public void gameStart(GameStartEvent gameStartEvent) {
+        GameMember memberOne = new GameMember("t1", " ", "descript1", 1400, Rank.BRONZE);
+        GameMember memberTwo = new GameMember("t2", " ", "descript2", 1400, Rank.GOLD);
+
+        //게임 ID 생성
+        String gameRoomId = UUID.randomUUID().toString();
+
+        //오픈비두 세션 생성, 토큰 생성
+        String openviduSessionId = openviduService.createSession(gameRoomId);
+        String openviduTokenOne = openviduService.createConnection(openviduSessionId);
+        String openviduTokenTwo = openviduService.createConnection(openviduSessionId);
+
+        //Redis 에 게임 객체 저장
+        GameRoom gameRoom = new GameRoom(gameRoomId, memberOne, memberTwo, GameStatus.BEFORE_START);
+        gameRoomRepository.save(gameRoom);
+    }
+}
