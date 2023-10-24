fe55b1d32a73396602d1165785f46c5baebfaed6
전영호 <aplblypt@gmail.com>
2023-10-24 14:24:03 +0900

    feat: 임시로 CustomHandshakeHandler 를 통해 Principal 인증기능 생성

--- backend/dgrr/src/main/java/live/dgrr/global/config/stomp/StompConfig.java
+++ backend/dgrr/src/main/java/live/dgrr/global/config/stomp/StompConfig.java
@@ -10,9 +10,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
+    //todo:추후 CustomHandshakeHandler 를 제거 후 Authentication 추가
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect").setAllowedOrigins("*");
+        registry.addEndpoint("/connect").setAllowedOrigins("*")
+                .setHandshakeHandler(new CustomHandshakeHandler());
     }
 
     @Override
