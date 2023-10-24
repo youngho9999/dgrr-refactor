8f94cf8ca54128e77b77b2ac80d04eb47f5a1c14
전영호 <aplblypt@gmail.com>
2023-10-24 15:48:34 +0900

    fix: GameRoom, Auditing 추가

--- backend/dgrr/src/main/java/live/dgrr/domain/game/entity/GameRoom.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/entity/GameRoom.java
@@ -2,12 +2,14 @@ package live.dgrr.domain.game.entity;
 
 import jakarta.persistence.Id;
 import live.dgrr.global.entity.BaseEntity;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
 
 @RedisHash(value = "gameRoom", timeToLive = 200)
-public class GameRoom extends BaseEntity {
+public class GameRoom {
 
     @Id
     private String gameRoomId;
@@ -20,4 +22,16 @@ public class GameRoom extends BaseEntity {
     private LocalDateTime secondRoundEndTime;
     private RoundResult firstRoundResult;
     private RoundResult secondRoundResult;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public GameRoom(String gameRoomId, GameMember memberOne, GameMember memberTwo, GameStatus gameStatus) {
+        this.gameRoomId = gameRoomId;
+        this.memberOne = memberOne;
+        this.memberTwo = memberTwo;
+        this.gameStatus = gameStatus;
+    }
 }
