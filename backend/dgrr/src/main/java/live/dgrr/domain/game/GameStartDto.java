1fa87e2f15bb285dbb54d6635bad6092ef7f9250
전영호 <aplblypt@gmail.com>
2023-10-24 16:28:50 +0900

    fix: GameStartDto gameSessionId -> gameRoomId 로 변경

--- backend/dgrr/src/main/java/live/dgrr/domain/game/GameStartDto.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/GameStartDto.java
@@ -2,6 +2,6 @@ package live.dgrr.domain.game;
 
 import live.dgrr.domain.game.entity.GameMember;
 
-public record GameStartDto(GameMember memberOne, GameMember memberTwo, String gameSessionId, String openviduToken,
+public record GameStartDto(GameMember memberOne, GameMember memberTwo, String gameRoomId, String openviduToken,
                            String turn) {
 }
