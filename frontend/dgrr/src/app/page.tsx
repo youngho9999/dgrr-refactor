2b0db4623169db300c71b6929b49d7d26cad4689
337ccc <337ccc@gmail.com>
2023-10-25 14:13:46 +0900

    feat: Header 제작(기능은 코드 복사와 뒤로 가기만 가능)

--- frontend/dgrr/src/app/page.tsx
+++ frontend/dgrr/src/app/page.tsx
@@ -1,10 +1,9 @@
-import Header from '@/components/elements/Header'
+
 
 export default function Home() {
   return (
     <div>
       로그인 전 화면
-      <Header headerType='GAME'></Header>
     </div>
   )
 }
