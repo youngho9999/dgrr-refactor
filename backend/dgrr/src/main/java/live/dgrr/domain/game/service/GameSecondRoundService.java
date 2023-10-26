4e40b4a92a70f1a7c8b79027109936ff6559af81
전영호 <aplblypt@gmail.com>
2023-10-26 17:23:36 +0900

    feat: GameSecondRoundService 생성

---
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/service/GameSecondRoundService.java
@@ -0,0 +1,9 @@
+package live.dgrr.domain.game.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameSecondRoundService {
+    public void prepareSecondRoundStart(String gameRoomId) {
+    }
+}
