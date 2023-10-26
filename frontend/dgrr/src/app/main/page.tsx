b99100cc75dcf7c03a79656ed88a17368b866ad3
hyuna <kook980316@gmail.com>
2023-10-26 15:05:26 +0900

    feat: header 추가

--- frontend/dgrr/src/app/main/page.tsx
+++ frontend/dgrr/src/app/main/page.tsx
@@ -4,12 +4,14 @@ import title from "@/../public/images/logo_title.png";
 import Image from "next/image";
 import { LinkButton } from "@/components/LinkButton";
 import { FuncButton } from "@/components/FuncButton";
+import Header from "@/components/elements/Header";
 
 const MainPage = () => {
   // 나중에 설명 모달 on/off 로직 추가할 예정
   const handleModal = () => {};
   return (
     <div className="flex flex-col items-center w-screen bg-main-blue">
+      <Header headerType="MAIN" />
       <Image alt="캐릭터" src={character} className="w-40 h-40" />
       <Image alt="타이틀" src={title} className="w-48" />
       <div className="flex flex-col w-full items-center space-y-6">
