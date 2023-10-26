7e57b71d52217455325655e3b4c2995dcce12b5c
337ccc <337ccc@gmail.com>
2023-10-26 15:23:00 +0900

    feat: 마이 프로필 상단 부분 완성

--- frontend/dgrr/src/app/myprofile/page.tsx
+++ frontend/dgrr/src/app/myprofile/page.tsx
@@ -13,13 +13,25 @@ const MyProfile = () => {
   });
 
   useEffect(() => {
-    setMyInfo({'nickname': '안녕', 'description': '행복한 하루 보내길', 'profileImage': './public/images/nongdam.jpg', 'rating': 1500, 'rank': 'Gold'})
+    setMyInfo({'nickname': '가나다라마바사아자차카타', 'description': '행복한 하루 보내길', 'profileImage': '/images/nongdam.jpg', 'rating': 1500, 'rank': 'Gold'})
   }, [])
 
   return (
     <div>
       <Header headerType='PROFILE' />
-      <img src='./public/images/nongdam.jpg' alt="" />
+      <div className='h-[220px] flex justify-center items-center'>
+        <div>
+          <div className='flex justify-center'>
+            <img src={myInfo.profileImage} alt='profileImage' className='w-[80px] aspect-square rounded-full' />
+          </div>
+          <div className='text-center mt-6 mb-3 text-lg font-semibold'>
+            {myInfo.nickname}
+          </div>
+          <div className='text-center text-sm text-[#767676]'>
+            {myInfo.description}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
