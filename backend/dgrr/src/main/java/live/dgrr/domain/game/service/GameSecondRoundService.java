86d62f2844cce8b0ed0309308cefa5033197a19a
전영호 <aplblypt@gmail.com>
2023-10-26 17:42:28 +0900

    feat: GameSecondRoundService 내 두번째 라운드 시작 로직

--- backend/dgrr/src/main/java/live/dgrr/domain/game/service/GameSecondRoundService.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/service/GameSecondRoundService.java
@@ -1,6 +1,11 @@
 package live.dgrr.domain.game.service;
 
 import live.dgrr.domain.game.entity.GameRoom;
+import live.dgrr.domain.game.entity.RoundResult;
+import live.dgrr.domain.game.entity.event.FirstRoundOverEvent;
+import live.dgrr.domain.game.entity.event.FirstRoundPreparedEvent;
+import live.dgrr.domain.game.entity.event.SecondRoundEndEvent;
+import live.dgrr.domain.game.entity.event.SecondRoundPreparedEvent;
 import live.dgrr.domain.game.repository.GameRoomRepository;
 import live.dgrr.global.exception.ErrorCode;
 import live.dgrr.global.exception.GameException;
@@ -8,12 +13,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Service
 @RequiredArgsConstructor
 public class GameSecondRoundService {
 
     private final GameRoomRepository gameRoomRepository;
-    private final ApplicationEventPublisher applicationEventPublisher;
+    private final ApplicationEventPublisher publisher;
+
+    private static final long ROUND_TIME = 5000L;
 
     /**
      * 2라운드 준비 신호
@@ -32,6 +43,21 @@ public class GameSecondRoundService {
     }
 
     private void secondRoundStart(String gameRoomId, GameRoom gameRoom) {
+        LocalDateTime now = LocalDateTime.now();
 
+        //todo: timer 추후 변경 필요
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                publisher.publishEvent(new SecondRoundEndEvent(gameRoomId, RoundResult.NO_LAUGH));
+            }
+        };
+
+        gameRoom.startSecondRound(now);
+        gameRoomRepository.save(gameRoom);
+        timer.schedule(timerTask,ROUND_TIME);
+
+        publisher.publishEvent(new SecondRoundPreparedEvent(gameRoom.getMemberOne().memberId(), gameRoom.getMemberTwo().memberId()));
     }
 }
