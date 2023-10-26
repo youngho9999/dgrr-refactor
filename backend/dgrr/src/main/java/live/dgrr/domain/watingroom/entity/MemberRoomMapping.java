407d900220bedbb6f76b13bb186a7f3096f27585
이수연 <sooyeon990828@naver.com>
2023-10-26 16:33:49 +0900

    feat: memberId 와 roomId를 매핑객체 생성

---
+++ backend/dgrr/src/main/java/live/dgrr/domain/watingroom/entity/MemberRoomMapping.java
@@ -0,0 +1,17 @@
+package live.dgrr.domain.watingroom.entity;
+
+import org.springframework.data.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash(value = "memberroommapping", timeToLive = 600)
+public class MemberRoomMapping {
+    @Id
+    private Long memberId;
+    private int roomId;
+}
---
+++ backend/dgrr/src/main/java/live/dgrr/domain/watingroom/repository/MemberRoomMappingRepository.java
@@ -0,0 +1,7 @@
+package live.dgrr.domain.watingroom.repository;
+
+import live.dgrr.domain.watingroom.entity.MemberRoomMapping;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRoomMappingRepository extends CrudRepository<MemberRoomMapping, Long> {
+}
