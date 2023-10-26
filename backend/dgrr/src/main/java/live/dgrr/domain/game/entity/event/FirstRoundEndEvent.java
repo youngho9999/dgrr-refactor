57f89600db72018fbde1395097ea3067b97f99f7
전영호 <aplblypt@gmail.com>
2023-10-26 16:55:31 +0900

    fix: FirstRoundEndEvent 파라미터 추가. String destination

--- backend/dgrr/src/main/java/live/dgrr/domain/game/entity/event/FirstRoundEndEvent.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/entity/event/FirstRoundEndEvent.java
@@ -2,5 +2,5 @@ package live.dgrr.domain.game.entity.event;
 
 import live.dgrr.domain.game.entity.RoundResult;
 
-public record FirstRoundEndEvent(String memberOneId, String memberTwoId, RoundResult roundResult) {
+public record FirstRoundEndEvent(String memberOneId, String memberTwoId, RoundResult roundResult, String destination) {
 }
