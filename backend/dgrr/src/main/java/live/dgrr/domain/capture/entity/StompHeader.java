15630b8ce42ea7cdfb9beea91d9e05aab5b8a883
itsdangerous <danger121@naver.com>
2023-10-27 17:22:44 +0900

    feat: 스톰프 헤더 결과 정의

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/entity/StompHeader.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/entity/StompHeader.java
@@ -1,4 +1,23 @@
 package live.dgrr.domain.capture.entity;
 
-public class ImageResult {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class StompHeader {
+    private int round;
+    private String gameSessionId;
+
+    @Override
+    public String toString() {
+        return "StompHeader : {" +
+                "round : " + Integer.toString(round) +
+                ", gameSessionId : " + gameSessionId
+                +"}";
+
+    }
 }
