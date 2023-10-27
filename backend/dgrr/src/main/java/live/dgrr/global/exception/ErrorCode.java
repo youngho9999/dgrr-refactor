e62dfa6ac6187bc33377610dc8ddf6fe47e92326
mjpark1329@naver.com <mjpark1329@naver.com>
2023-10-27 16:14:35 +0900

    fix: 커스텀 exception errorcode 추가(전영호 코드리뷰 반영)

--- backend/dgrr/src/main/java/live/dgrr/global/exception/ErrorCode.java
+++ backend/dgrr/src/main/java/live/dgrr/global/exception/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     IS_NOT_ROOM_MANAGER(HttpStatus.FORBIDDEN, "Is Not Room Manager"),
     NOT_ENOUGH_MEMBERS_TO_START(HttpStatus.BAD_REQUEST, "Not Enough Members To Start"), //대기 방에 2명 미만인데 시작을 누른경우
     MEMBER_NOT_READY_YET(HttpStatus.FORBIDDEN, "Member Not Ready Yet"), //대기 방에 있는 사람이 준비상태가 아닌 경우
+    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "File Not Found"), // yml 파일 등이 없을 때
     WAITING_ROOM_ALREADY_START(HttpStatus.BAD_REQUEST, "WaitingRoom Already Start");
     private final HttpStatus httpStatus;
     private final String message;
