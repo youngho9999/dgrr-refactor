cf04dbe2eb441ccc9810a25c77ade94c4a8935a1
전영호 <aplblypt@gmail.com>
2023-10-27 14:09:39 +0900

    fix: SecondRoundEndEvent.java -> SecondRoundEndEvent 명칭 변경

--- backend/dgrr/src/main/java/live/dgrr/domain/game/service/GameSecondRoundService.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/service/GameSecondRoundService.java
@@ -1,16 +1,15 @@
 package live.dgrr.domain.game.service;
 
 import live.dgrr.domain.game.entity.GameRoom;
+import live.dgrr.domain.game.entity.GameStatus;
 import live.dgrr.domain.game.entity.RoundResult;
-import live.dgrr.domain.game.entity.event.FirstRoundOverEvent;
-import live.dgrr.domain.game.entity.event.FirstRoundPreparedEvent;
-import live.dgrr.domain.game.entity.event.SecondRoundEndEvent;
-import live.dgrr.domain.game.entity.event.SecondRoundPreparedEvent;
+import live.dgrr.domain.game.entity.event.*;
 import live.dgrr.domain.game.repository.GameRoomRepository;
 import live.dgrr.global.exception.ErrorCode;
 import live.dgrr.global.exception.GameException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -50,7 +49,7 @@ public class GameSecondRoundService {
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                publisher.publishEvent(new SecondRoundEndEvent(gameRoomId, RoundResult.NO_LAUGH));
+                publisher.publishEvent(new SecondRoundOverEvent(gameRoomId, RoundResult.NO_LAUGH));
             }
         };
 
@@ -59,5 +58,34 @@ public class GameSecondRoundService {
         timer.schedule(timerTask,ROUND_TIME);
 
         publisher.publishEvent(new SecondRoundPreparedEvent(gameRoom.getMemberOne().memberId(), gameRoom.getMemberTwo().memberId()));
+    }
+
+    /**
+     * 2 라운드 종료 메소드
+     * @param event 2라운드 종료 정보 담긴 이벤트
+     */
+    @EventListener
+    public void firstRoundOver(SecondRoundOverEvent event) {
+        GameRoom gameRoom = gameRoomRepository.findById(event.gameRoomId())
+                .orElseThrow(() -> new GameException(ErrorCode.GAME_ROOM_NOT_FOUND));
+
+        //이미 웃어서 라운드 종료 된경우
+        if(gameRoom.getGameStatus().equals(GameStatus.SECOND_ROUND)) {
+            return;
+        }
+
+        gameRoom.finishFirstRound(event.roundResult());
+        gameRoomRepository.save(gameRoom);
+
+        String destination;
+        if(event.roundResult().equals(RoundResult.LAUGH)) {
+            destination = FIRST_ROUND_LAUGH;
+        }
+        else {
+            destination = FIRST_ROUND_NO_LAUGH;
+        }
+
+        publisher.publishEvent(new FirstRoundEndEvent(gameRoom.getMemberOne().memberId(),
+                gameRoom.getMemberTwo().memberId(), gameRoom.getFirstRoundResult(), destination));
     }
 }
