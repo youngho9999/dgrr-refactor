f1078adf205233d8acc0749ac130cff90680287a
itsdangerous <danger121@naver.com>
2023-10-27 17:22:30 +0900

    feat: 이미지 결과 객체 정의

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/entity/ImageResult.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/entity/ImageResult.java
@@ -1,2 +1,26 @@
-package live.dgrr.domain.capture.entity;public class ImageResult {
+package live.dgrr.domain.capture.entity;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ImageResult {
+
+    private final boolean success;
+    private String emotion;
+    private double probability;
+    private double smileProbability;
+    private String encodedImage;
+    private StompHeader stompHeader;
+
+    public String printField() {
+        return "success : " + success +
+                ", emotion : " + emotion +
+                ", probability : " + probability +
+                ", smileProbability : " + smileProbability +
+                ", encodedImage : " + (encodedImage == null ? "null, " : encodedImage.substring(0, Math.min(40, encodedImage.length()))) +
+                stompHeader.toString() +
+                "}";
+    }
 }
