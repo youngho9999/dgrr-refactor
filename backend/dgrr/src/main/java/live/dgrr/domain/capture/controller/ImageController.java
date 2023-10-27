429d7e0416f19fc476c59a93d2c2daba00f53ee7
itsdangerous <danger121@naver.com>
2023-10-27 17:19:23 +0900

    feat: 파이썬 서버로부터 이미지 결과 받는 기능 구현

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/controller/ImageController.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/controller/ImageController.java
@@ -1,2 +1,22 @@
-package live.dgrr.domain.caputre.controller;public class ImageController {
+package live.dgrr.domain.capture.controller;
+
+import live.dgrr.domain.capture.service.ImageProcessingService;
+import org.json.simple.parser.ParseException;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageController {
+    private final ImageProcessingService imageProcessingService;
+
+    public ImageController(ImageProcessingService imageProcessingService) {
+        this.imageProcessingService = imageProcessingService;
+    }
+
+    @MessageMapping("/capture-image")
+    public void receiveImageDataFromPythonClient(@Payload String img)throws ParseException {
+        imageProcessingService.processingCaptureImage(img);
+    }
 }
