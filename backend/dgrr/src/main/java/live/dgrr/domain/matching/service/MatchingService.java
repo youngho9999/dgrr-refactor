789bdbeadacbb591995f05105f5c8dc5595241de
이수연 <sooyeon990828@naver.com>
2023-10-30 17:09:54 +0900

    feat: 랜덤매칭 요청 시 로직 구현

---
+++ backend/dgrr/src/main/java/live/dgrr/domain/matching/service/MatchingService.java
@@ -0,0 +1,31 @@
+package live.dgrr.domain.matching.service;
+
+import live.dgrr.domain.matching.repository.MatchingRepository;
+import live.dgrr.domain.waitingroom.entity.GameStartEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MatchingService {
+
+    private final MatchingRepository matchingRepository;
+    private final ApplicationEventPublisher publisher;
+
+    public void matchRandom(String memberId) {
+        log.info("MatchingService - matchRandom : isMemberSetEmpty : {}", matchingRepository.isMemberSetEmpty());
+
+        if(!matchingRepository.isMemberSetEmpty() && !matchingRepository.isFirstMember(memberId)) {
+            //member가 있고 제일 첫번째가 본인이 아니면 매칭해서 보내기
+            publisher.publishEvent(new GameStartEvent(matchingRepository.publishFirstMember(),memberId));
+        }else {
+            //사용자 넣기
+            matchingRepository.addMember(memberId, (double) System.currentTimeMillis());
+        }
+
+    }
+}
