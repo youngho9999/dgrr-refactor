6d09477afc9fdd51ea1703e0897919c35c0fe82e
337ccc <337ccc@gmail.com>
2023-10-30 14:09:23 +0900

    feat: 최근 전적 페이지 만듦

--- frontend/dgrr/src/app/myprofile/page.tsx
+++ frontend/dgrr/src/app/myprofile/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Header from '@/components/elements/Header';
+import Link from "next/link";
 import Rank from '@/components/elements/Rank';
 import { IoChevronForwardOutline } from 'react-icons/io5';
 import RecentRecordItem from '@/components/elements/RecentRecordItem';
@@ -146,7 +147,9 @@ const MyProfile = () => {
           {/* 전적이 아직 하나도 없다면 더 보기 버튼 생기지 않음 */}
           {myInfo.gameDetailList.length !== 0 ? (
             <div className='flex cursor-pointer gap-x-[3px]'>
+              <Link href='/myprofile/recent-record'>
               <div className='text-sm font-bold inline-block'>더 보기</div>
+              </Link>
               <div className='inline-block'>
                 <IoChevronForwardOutline fontSize={'18px'} />
               </div>
---
+++ frontend/dgrr/src/app/myprofile/recent-record/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Header from '@/components/elements/Header';
+
+const RecentRecord = () => {
+  return (
+    <div>
+      <Header headerType='OTHER'>최근 전적</Header>
+    </div>
+  )
+}
+
+export default RecentRecord;
