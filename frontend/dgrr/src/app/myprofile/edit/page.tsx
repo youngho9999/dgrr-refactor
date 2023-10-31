d0686b09a558a1516b33990a498faba90ff8ef53
337ccc <337ccc@gmail.com>
2023-10-31 11:03:32 +0900

    feat: 프로필 수정 페이지 제작

---
+++ frontend/dgrr/src/app/myprofile/edit/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Header from '@/components/elements/Header';
+
+const Edit = () => {
+  return (
+    <div>
+      <Header headerType='OTHER'>프로필 수정</Header>
+    </div>
+  )
+}
+
+export default Edit;
