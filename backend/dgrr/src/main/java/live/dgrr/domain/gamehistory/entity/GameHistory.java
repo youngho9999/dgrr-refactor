7775e4ef90476e10807de7b485c7dda1017ef9cb
이수연 <sooyeon990828@naver.com>
2023-10-31 16:11:07 +0900

    feat: GameHistory 객체 생성

---
+++ backend/dgrr/src/main/java/live/dgrr/domain/gamehistory/entity/GameHistory.java
@@ -0,0 +1,32 @@
+package live.dgrr.domain.gamehistory.entity;
+
+import jakarta.persistence.*;
+import live.dgrr.domain.game.entity.GameResult;
+import live.dgrr.domain.game.entity.event.GameType;
+import live.dgrr.domain.member.entity.Member;
+import live.dgrr.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GameHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gameHistoryId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")
+    private Member member;
+    private String gameRoomId;
+    private GameResult gameResult; //WIN, LOSE, DRAW
+    private GameType gameType; //RANDOM, PRIVATE
+    private Long gameTime;
+    private Integer holdingTime;
+    private String highlightImage;
+
+}
