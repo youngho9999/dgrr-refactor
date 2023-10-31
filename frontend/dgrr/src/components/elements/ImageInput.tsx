a5283f4a973c2a6d37a0a952cbaed5687cd5bd8e
337ccc <337ccc@gmail.com>
2023-10-31 18:00:06 +0900

    feat: 사진 저장 만드는 중

--- frontend/dgrr/src/components/elements/ImageInput.tsx
+++ frontend/dgrr/src/components/elements/ImageInput.tsx
@@ -1,25 +1,38 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IoCamera } from 'react-icons/io5';
 
 interface ImageInputProps {
-  myProfileImage: string,
+  myProfileImage: string;
 }
 
 const ImageInput = ({ myProfileImage }: ImageInputProps) => {
-  const [nowProfileImage, setNowProfileImage] = useState(myProfileImage)
-  
+  const [nowProfileImage, setNowProfileImage] = useState(myProfileImage);
+  const changeProfileImage = (event: any) => {
+    const file = event.target.files
+    console.log(file)
+    setNowProfileImage(file)
+  }
+
   return (
     <div className='px-6 py-8'>
       <div className='w-[121px] relative'>
-        <img className='w-[121px] aspect-square rounded-full' src={nowProfileImage} alt="기본 사진" />
-        <div className='absolute bottom-0 right-0 bg-[#E0E0E0] w-[25px] aspect-square rounded-full flex justify-center items-center cursor-pointer hover:text-main-blue'>
+        <img
+          className='w-[121px] aspect-square rounded-full'
+          src={nowProfileImage}
+          alt='기본 사진'
+        />
+        <label
+          htmlFor='fileInput'
+          className='absolute bottom-0 right-0 bg-[#E0E0E0] w-[25px] aspect-square rounded-full flex justify-center items-center cursor-pointer hover:text-main-blue'
+        >
           <IoCamera fontSize={'17px'} />
-        </div>
+        </label>
+        <input type='file' id='fileInput' style={{ display: 'none' }} accept='img/*' onChange={changeProfileImage} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ImageInput;
