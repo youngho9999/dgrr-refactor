8783fcfb8a33b7d1827466d2c59b53d703670121
hyuna333 <kook980316@gmail.com>
2023-11-01 01:23:16 +0900

    feat: 게임 설명 모달 추가

--- frontend/dgrr/src/app/game/loading/page.tsx
+++ frontend/dgrr/src/app/game/loading/page.tsx
@@ -3,6 +3,7 @@ import character from "@/../../public/images/logo_character.png";
 import Image from "next/image";
 import { FuncButton } from "@/components/FuncButton";
 import { useState } from "react";
+import { ExplainModal } from "@/components/elements/ExplainModal";
 
 const GameLoading = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -11,6 +12,7 @@ const GameLoading = () => {
   };
   return (
     <div className="grid place-items-center bg-main-blue w-screen h-screen max-w-[500px]">
+      {openModal && <ExplainModal onClose={handleModal} />}
       <Image alt="캐릭터" src={character} className="w-40 h-40" />
       <h1>게임 찾는 중...</h1>
       <FuncButton value="게임 설명" clickEvent={handleModal} small={true} />
--- frontend/dgrr/src/app/main/page.tsx
+++ frontend/dgrr/src/app/main/page.tsx
@@ -5,13 +5,18 @@ import Image from "next/image";
 import { LinkButton } from "@/components/LinkButton";
 import { FuncButton } from "@/components/FuncButton";
 import Header from "@/components/elements/Header";
+import { ExplainModal } from "@/components/elements/ExplainModal";
+import { useState } from "react";
 
 const MainPage = () => {
-  // 나중에 설명 모달 on/off 로직 추가할 예정
-  const handleModal = () => {};
+  const [openModal, setOpenModal] = useState(false)
+  const handleModal = () => {
+    setOpenModal(!openModal)
+  };
   return (
     <div className="flex flex-col items-center w-screen bg-main-blue">
       <Header headerType="MAIN" />
+      {openModal && <ExplainModal onClose={handleModal} />}
       <Image alt="캐릭터" src={character} className="w-40 h-40" />
       <Image alt="타이틀" src={title} className="w-48" />
       <div className="flex flex-col w-full items-center space-y-6">
