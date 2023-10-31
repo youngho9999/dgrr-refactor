acfcd6127e44917121c974c6d8ae747369854894
itsdangerous <danger121@naver.com>
2023-10-31 16:44:09 +0900

    feat: JSON 역직렬화 및 웃었을 경우 처리 로직 구현

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/service/CaptureProcessingService.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/service/CaptureProcessingService.java
@@ -3,49 +3,53 @@ package live.dgrr.domain.capture.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import live.dgrr.domain.capture.entity.CaptureResult;
-import live.dgrr.domain.game.entity.GameMember;
-import live.dgrr.domain.game.entity.GameRoom;
-import live.dgrr.domain.game.repository.GameRoomRepository;
+import live.dgrr.domain.game.entity.RoundResult;
+import live.dgrr.domain.game.entity.event.FirstRoundOverEvent;
+import live.dgrr.domain.game.entity.event.SecondRoundOverEvent;
 import live.dgrr.global.exception.ErrorCode;
 import live.dgrr.global.exception.GameException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CaptureProcessingService {
+    private final ApplicationEventPublisher publisher;
+    private final ObjectMapper objectMapper;
 
-    private static final String CAPTURE_RESULT_DEST = "recv/cpature-result";
-    private final GameRoomRepository gameRoomRepository;
-    private final SimpMessagingTemplate template;
+    public static final int FIRST_ROUND = 1;
+    public static final int SECOND_ROUND = 2;
 
-    public void processingCaptureImage(String data) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        CaptureResult captureResult = objectMapper.readValue(data, CaptureResult.class);
-        log.info("Capture Result : {}", captureResult);
+    public CaptureResult deSerializationCapture(String capture) {
+        try {
+            CaptureResult captureResult = objectMapper.readValue(capture, CaptureResult.class);
 
+            log.info(capture);
+
+            if (captureResult.getEncodedImage() != null) {
+                publishingLaugh(captureResult);
+            }
+
+            return captureResult;
+        } catch (JsonProcessingException e) {
+            throw new GameException(ErrorCode.IMAGE_PROCESSING_ERROR);
+        }
+    }
+
+    public void publishingLaugh(CaptureResult captureResult) {
         String gameSessionId = captureResult.getHeader().getGameSessionId();
-        sendCaptureResult(gameSessionId, captureResult);
+        int round = captureResult.getHeader().getRound();
+
+        if (round == FIRST_ROUND) {
+            publisher.publishEvent(new FirstRoundOverEvent(gameSessionId, RoundResult.LAUGH));
+        }
+        else if (round == SECOND_ROUND) {
+            publisher.publishEvent(new SecondRoundOverEvent(gameSessionId, RoundResult.LAUGH));
+        }
     }
 
-    public void sendCaptureResult(String gameSessionId, CaptureResult captureResult) {
-        GameRoom gameRoom = gameRoomRepository.findById(gameSessionId).orElseThrow(() -> new GameException(ErrorCode.GAME_ROOM_NOT_FOUND));
-
-        GameMember memberOne = gameRoom.getMemberOne();
-        GameMember memberTwo = gameRoom.getMemberTwo();
-
-        if (memberOne != null && memberTwo != null) {
-            template.convertAndSendToUser(memberOne.memberId(), CAPTURE_RESULT_DEST, captureResult);
-            template.convertAndSendToUser(memberTwo.memberId(), CAPTURE_RESULT_DEST, captureResult);
-        }
-        else {
-            throw new GameException(ErrorCode.MEMBER_NOT_FOUND);
-        }
-
-    }
 }
