7df77d415dc9874fbb8739f5fc1d68690c4a4d1c
전영호 <aplblypt@gmail.com>
2023-10-31 16:04:44 +0900

    test: 상대방 탈주시 결과 테스트

--- backend/dgrr/src/test/java/live/dgrr/domain/game/service/GameSecondRoundServiceTest.java
+++ backend/dgrr/src/test/java/live/dgrr/domain/game/service/GameSecondRoundServiceTest.java
@@ -6,7 +6,6 @@ import live.dgrr.domain.game.entity.*;
 import live.dgrr.domain.game.entity.event.*;
 import live.dgrr.domain.game.repository.GameRoomRepository;
 import live.dgrr.global.entity.Rank;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -18,6 +17,7 @@ import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
@@ -159,6 +159,24 @@ class GameSecondRoundServiceTest {
 
         //then
         assertThat(result.gameResult()).isEqualTo(GameResult.WIN);
+    }
+
+    @Test
+    void 상대방_탈주() {
+        //given
+        doAnswer(invocation -> {
+            GameMember memberOne = new GameMember(memberOneId, "Player1", "jpg", "This is description", 1500, Rank.SILVER);
+            GameMember memberTwo = new GameMember(memberTwoId, "Player2", "jpg", "This is description", 1200, Rank.BRONZE);
+            GameRoom gameRoom = new GameRoom(gameRoomId, memberOne, memberTwo, GameStatus.FIRST_ROUND);
+            return Optional.of(gameRoom);
+        }).when(gameRoomRepository).findById(gameRoomId);
+
+        //when
+        GameResultResponse result = gameSecondRoundService.leaveGame(memberOneId, gameRoomId);
+
+        //then
+        assertThat(result.gameResult()).isEqualTo(GameResult.WIN);
+        assertThat(result.myInfo().memberId()).isEqualTo(memberTwoId);
     }
 
     private void mockGameRoom(RoundResult firstRoundResult, RoundResult secondRoundResult) {
