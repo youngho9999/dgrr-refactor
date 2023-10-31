6b3fd1c10b1b9f7559c97490520fd2c141100476
itsdangerous <danger121@naver.com>
2023-10-31 16:43:19 +0900

    feat: gameRoomId를 가진 클라이언트에게 이미지 결과 보내는 로직을 컨트롤러로 뺌

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/controller/CaptureController.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/controller/CaptureController.java
@@ -1,21 +1,37 @@
 package live.dgrr.domain.capture.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import live.dgrr.domain.capture.entity.CaptureResult;
 import live.dgrr.domain.capture.service.CaptureProcessingService;
+import live.dgrr.domain.game.entity.GameRoom;
+import live.dgrr.domain.game.repository.GameRoomRepository;
+import live.dgrr.global.exception.ErrorCode;
+import live.dgrr.global.exception.GameException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CaptureController {
-    private final CaptureProcessingService imageProcessingService;
+@RequiredArgsConstructor
 
-    public CaptureController(CaptureProcessingService captureProcessingService) {
-        this.imageProcessingService = captureProcessingService;
-    }
+public class CaptureController {
+    private final CaptureProcessingService captureProcessingService;
+    private final GameRoomRepository gameRoomRepository;
+    private final SimpMessagingTemplate template;
+    private static final String CAPTURE_RESULT_DEST = "recv/cpature-result";
+
 
     @MessageMapping("/capture-image")
-    public void receiveImageDataFromPythonClient(@Payload String img) throws JsonProcessingException {
-        imageProcessingService.processingCaptureImage(img);
+    public void sendCaptureResultToUsers(@Payload String capture) {
+        CaptureResult captureResult = captureProcessingService.deSerializationCapture((capture));
+        String gameSessionId = captureResult.getHeader().getGameSessionId();
+        GameRoom gameRoom = gameRoomRepository.findById(gameSessionId).orElseThrow(() -> new GameException(ErrorCode.GAME_ROOM_NOT_FOUND));
+
+        template.convertAndSendToUser(gameRoom.getMemberOne().memberId(), CAPTURE_RESULT_DEST, captureResult);
+        template.convertAndSendToUser(gameRoom.getMemberTwo().memberId(), CAPTURE_RESULT_DEST, captureResult);
+
     }
+
 }
