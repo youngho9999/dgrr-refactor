73140e3bc117a6c983cb98c0682829666fbcad1d
itsdangerous <danger121@naver.com>
2023-11-01 15:15:49 +0900

    feat: 이벤트 리스너로 캡처 이미지 분석 결과 처리하기

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/controller/CaptureController.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/controller/CaptureController.java
@@ -1,12 +1,15 @@
 package live.dgrr.domain.capture.controller;
 
 import live.dgrr.domain.capture.entity.CaptureResult;
+import live.dgrr.domain.capture.entity.event.CaptureResultEvent;
 import live.dgrr.domain.capture.service.CaptureService;
 import live.dgrr.domain.game.entity.GameRoom;
+import live.dgrr.domain.game.entity.event.FirstRoundPreparedEvent;
 import live.dgrr.domain.game.repository.GameRoomRepository;
 import live.dgrr.global.exception.ErrorCode;
 import live.dgrr.global.exception.GameException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -17,20 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class CaptureController {
     private final CaptureService captureService;
-    private final GameRoomRepository gameRoomRepository;
     private final SimpMessagingTemplate template;
     private static final String CAPTURE_RESULT_DEST = "recv/cpature-result";
 
 
     @MessageMapping("/capture-image")
-    public void sendCaptureResultToUsers(@Payload String capture) {
-        CaptureResult captureResult = captureService.deSerializationCapture((capture));
-        String gameSessionId = captureResult.getHeader().getGameSessionId();
-        GameRoom gameRoom = gameRoomRepository.findById(gameSessionId).orElseThrow(() -> new GameException(ErrorCode.GAME_ROOM_NOT_FOUND));
+    public void receiveCapture(@Payload String capture) {
+        captureService.deSerializationCapture(capture);
 
-        template.convertAndSendToUser(gameRoom.getMemberOne().memberId(), CAPTURE_RESULT_DEST, captureResult);
-        template.convertAndSendToUser(gameRoom.getMemberTwo().memberId(), CAPTURE_RESULT_DEST, captureResult);
+    }
 
+    @EventListener
+    public void sendCaptureResultToUsers(CaptureResultEvent event) {
+        template.convertAndSendToUser(event.memberOneId(), CAPTURE_RESULT_DEST, event.captureResult());
+        template.convertAndSendToUser(event.memberTwoId(), CAPTURE_RESULT_DEST, event.captureResult());
     }
 
 }
