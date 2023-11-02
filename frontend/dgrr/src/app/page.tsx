3eaf6a523d3773b9f947bba105b4467fb0cb6494
337ccc <337ccc@gmail.com>
2023-11-02 17:36:05 +0900

    refactor: 코드 수정

--- frontend/dgrr/src/app/page.tsx
+++ frontend/dgrr/src/app/page.tsx
@@ -1,12 +1,16 @@
 export default function Home() {
   return (
-    <div className='bg-main-blue w-screen h-screen max-w-[360px]'>
-      <div className='flex flex-col justify-between py-36 h-full'>
-        <div className='px-3'>
-          <img src='/images/login_logo.png' alt='로그인 전 화면에서 사용할 로고' />
+    <div className="bg-main-blue w-screen h-screen max-w-[360px]">
+      <div className="flex flex-col justify-between py-36 h-full">
+        <div className="px-3">
+          <img src="/images/login_logo.png" alt="로그인 전 화면에서 사용할 로고" />
         </div>
-        <div className='px-4'>
-          <img src='/images/kakao_login.png' alt='카카오 로그인 버튼' className='cursor-pointer hover:brightness-110' />
+        <div className="px-4">
+          <img
+            src="/images/kakao_login.png"
+            alt="카카오 로그인 버튼"
+            className="cursor-pointer hover:brightness-110"
+          />
         </div>
       </div>
     </div>
