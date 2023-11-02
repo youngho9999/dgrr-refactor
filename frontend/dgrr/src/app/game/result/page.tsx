3be1cc74bb948c95f32b124defdc55b258258525
337ccc <337ccc@gmail.com>
2023-11-02 14:19:55 +0900

    design: 게임 결과 틀 구성

--- frontend/dgrr/src/app/game/result/page.tsx
+++ frontend/dgrr/src/app/game/result/page.tsx
@@ -1,7 +1,8 @@
 const Result = () => {
   return (
-    <div>
-      게임 결과
+    <div className="flex justify-center items-center bg-main-blue w-screen h-screen max-w-[360px]">
+      <div className='bg-white w-11/12 h-[522px] rounded-[12px]'>
+      </div>
     </div>
   )
 }
