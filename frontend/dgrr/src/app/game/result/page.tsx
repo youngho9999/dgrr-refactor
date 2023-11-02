2d077276efe782446c7c468d80f58b62d81ba152
337ccc <337ccc@gmail.com>
2023-11-02 15:40:39 +0900

    feat: 게임 결과 화면의 구성 요소들 삽입

--- frontend/dgrr/src/app/game/result/page.tsx
+++ frontend/dgrr/src/app/game/result/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import { FuncButton } from '@/components/FuncButton';
 import Rank from '@/components/elements/Rank';
-import ProgressBar from '@ramonak/react-progress-bar';
 import { useEffect, useState } from 'react';
 
 const Result = () => {
@@ -26,23 +26,27 @@ const Result = () => {
     afterTier: '',
   });
 
+  const clickOneMore = () => {
+    console.log('One More Time')
+  }
+
   useEffect(() => {
     setGameResult({
       myInfo: {
         nickname: 'player1',
-        profileImage: 'https://example.com/images/player1.jpg',
+        profileImage: '/images/nongdam.jpg',
         description: 'A dedicated gamer from Seoul.',
         rating: 1500,
         tier: 'Gold',
       },
       enemyInfo: {
-        nickname: 'player2',
-        profileImage: 'https://example.com/images/player2.jpg',
+        nickname: '가나다라마바사아자차카타',
+        profileImage: '/images/sample_image2.png',
         description: 'An avid player from Busan.',
         rating: 1550,
         tier: 'Gold',
       },
-      highlightImage: 'https://example.com/images/player2.jpg', //비겼을 경우 null
+      highlightImage: '/images/sample_image2.png', //비겼을 경우 null
       gameResult: 'LOSE',
       afterRating: 1600,
       afterTier: 'Gold',
@@ -62,6 +66,17 @@ const Result = () => {
         <div className='mt-16'>
           <Rank pageType='GAMERESULT' rating={gameResult.afterRating} tier={gameResult.afterTier} />
         </div>
+        <div>
+          <div className='flex gap-2'>
+            <img className='w-10 aspect-square rounded-full items-center' src={gameResult.enemyInfo.profileImage} alt='상대방 프로필 사진' />
+            <div className='inline-block'>
+              <div className='h-[16px]'>{gameResult.enemyInfo.nickname}</div>
+              <div className='h-[16px]'>{gameResult.enemyInfo.description}</div>
+            </div>
+          </div>
+        </div>
+        <FuncButton clickEvent={clickOneMore} value='한 판 더?'></FuncButton>
+        <FuncButton clickEvent={clickOneMore} value='한 판 더?'></FuncButton>
       </div>
     </div>
   );
