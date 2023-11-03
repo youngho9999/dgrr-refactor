b53d10a25971f8a8624bc1dd973f56a78c2b845e
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-03 16:03:48 +0900

    feat: 전 시즌 랭킹도 받아올 수 있도록 rankingController 수정

--- backend/dgrr/src/main/java/live/dgrr/domain/ranking/controller/RankingController.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/ranking/controller/RankingController.java
@@ -17,9 +17,9 @@ public class RankingController {
     private final RankingService rankingService;
 
     // member id 기준 ranking 조회
-    @GetMapping("/member-id/{memberId}") // TODO: 로그인 구현 후 token 값 가져오는 걸로 바꾸기
-    public ResponseEntity<RankingMemberResponse> getRankingByMemberId(@PathVariable Long memberId) {
-        RankingMemberResponse response = rankingService.getRankingMember(memberId);
+    @GetMapping("/member-id/{memberId}/{season}") // TODO: 로그인 구현 후 token 값 가져오는 걸로 바꾸기
+    public ResponseEntity<RankingMemberResponse> getRankingByMemberId(@PathVariable(value="memberId") Long memberId, @PathVariable(value="season") Season season) {
+        RankingMemberResponse response = rankingService.getRankingMember(memberId, season);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
