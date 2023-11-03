46e6be9c6d90db96a1d0a1845c7c57a64252e469
hyuna <kook980316@gmail.com>
2023-11-03 17:38:23 +0900

    fix: 경로 수정

--- frontend/dgrr/src/app/game/list/page.tsx
+++ frontend/dgrr/src/app/game/list/page.tsx
@@ -1,18 +1,18 @@
-"use client";
-import Header from "@/components/elements/Header";
-import Image from "next/image";
-import doorImg from "@/../public/images/door.svg";
-import keyImg from "@/../public/images/key.svg";
-import welcomeImg from "@/../public/images/welcome.svg";
-import Link from "next/link";
-import { FindRoomModal } from "./FindRoomModal";
-import { useState } from "react";
+'use client';
+import Header from '@/components/elements/Header';
+import Image from 'next/image';
+import doorImg from '@/../public/images/door.svg';
+import keyImg from '@/../public/images/key.svg';
+import welcomeImg from '@/../public/images/welcome.svg';
+import Link from 'next/link';
+import { FindRoomModal } from './FindRoomModal';
+import { useState } from 'react';
 
 const ListPage = () => {
   const gameList = [
-    { value: '방 만들기', imgLink: doorImg, navLink: 'game/room' },
+    { value: '방 만들기', imgLink: doorImg, navLink: '/game/room' },
     { value: '방 찾기', imgLink: keyImg, navLink: '' },
-    { value: '랜덤 매칭', imgLink: welcomeImg, navLink: 'game/loading' },
+    { value: '랜덤 매칭', imgLink: welcomeImg, navLink: '/game/loading' },
   ];
   const commonClass =
     'w-4/5 max-w-xs py-3 mx-auto space-y-3 bg-white rounded-lg border-2 border-black flex flex-col items-center hover:brightness-125';
