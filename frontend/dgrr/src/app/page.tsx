73e364472c7aa94e90686b64f03df91c31938b5e
337ccc <337ccc@gmail.com>
2023-11-03 09:16:15 +0900

    design: 로그인 전 화면 너비 변경

--- frontend/dgrr/src/app/page.tsx
+++ frontend/dgrr/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-main-blue w-screen h-screen max-w-[360px]">
+    <div className="bg-main-blue w-screen h-screen max-w-[500px]">
       <div className="flex flex-col justify-between py-36 h-full">
         <div className="px-3">
           <img src="/images/login_logo.png" alt="로그인 전 화면에서 사용할 로고" />
