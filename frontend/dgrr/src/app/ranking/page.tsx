dcb61f69a9d15d8cd8c0e1d8ac35e89c45355cc3
337ccc <337ccc@gmail.com>
2023-11-03 13:29:07 +0900

    feat: 랭킹 조회 페이지 작성

---
+++ frontend/dgrr/src/app/ranking/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/elements/Header';
+
+const Ranking = () => {
+  return (
+    <div className='w-screen max-w-[500px]'>
+      <Header headerType='OTHER'>랭킹 조회</Header>
+    </div>
+  )
+}
+
+export default Ranking;
