d4b5f639054bda6068d42706a4b75eeba869c23c
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-03 14:01:16 +0900

    feat: 카카오 소셜 로그인 로직을 위한 findByKakaoId 메서드 추가

--- backend/dgrr/src/main/java/live/dgrr/domain/member/repository/MemberRepository.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/member/repository/MemberRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     List<Member> findByNickname(String nickname);
+
+    Member findByKakaoId(String kakaoId);
 }
