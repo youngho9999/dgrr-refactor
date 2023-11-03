25229f82307a4fe0229b1536b1bcd6d9c38dab5b
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-03 16:05:17 +0900

    feat: 리턴 값에 닉네임과 프로필 사진도 포함할 수 있도록 RankingResponse 수정

--- backend/dgrr/src/main/java/live/dgrr/domain/ranking/dto/response/RankingResponse.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/ranking/dto/response/RankingResponse.java
@@ -3,12 +3,14 @@ package live.dgrr.domain.ranking.dto.response;
 import lombok.Builder;
 
 @Builder
-public record RankingResponse(Long memberId, Double rating, Long rank) {
-    public static RankingResponse of(Long memberId, Double rating, Long rank) {
+public record RankingResponse(Long memberId, Double rating, Long rank, String nickname, String profileImage) {
+    public static RankingResponse of(Long memberId, Double rating, Long rank, String nickname, String profileImage) {
         return RankingResponse.builder()
                 .memberId(memberId)
                 .rating(rating)
                 .rank(rank)
+                .nickname(nickname)
+                .profileImage(profileImage)
                 .build();
     }
 }
