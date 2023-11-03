64be00c9285490aadc71aa002cf7797c47aa6721
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-03 14:44:58 +0900

    fix: optional로 바꿔서 null pointer exception 대비(전영호 코드리뷰 반영)

--- backend/dgrr/src/main/java/live/dgrr/domain/member/repository/MemberRepository.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/member/repository/MemberRepository.java
@@ -4,9 +4,10 @@ import live.dgrr.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     List<Member> findByNickname(String nickname);
 
-    Member findByKakaoId(String kakaoId);
+    Optional<Member> findByKakaoId(String kakaoId);
 }
