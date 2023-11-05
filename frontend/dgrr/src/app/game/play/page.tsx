2c08f1504d3bee6f2685425c9eb5d1ce1d93b557
hyuna333 <kook980316@gmail.com>
2023-11-06 00:29:20 +0900

    feat: 웹소켓 관련 로직 추가

--- frontend/dgrr/src/app/game/play/page.tsx
+++ frontend/dgrr/src/app/game/play/page.tsx
@@ -1,54 +1,123 @@
 'use client';
-import { useAppSelector } from "@/store/hooks";
-import { stompConfig } from "@/types/game";
-import { useState } from "react";
+import { useAppSelector } from '@/store/hooks';
+import { stompConfig } from '@/types/game';
+import { useEffect, useRef, useState } from 'react';
 
 const PlayPage = () => {
-  const client = useAppSelector((state)=>state.game.client)
+  const client = useAppSelector((state) => state.game.client);
   const { DESTINATION_URI } = stompConfig;
-  const { FIRST_ROUND_GO_URI, FIRST_ROUND_NO_LAUGH_URI, FIRST_ROUND_LAUGH_URI, FIRST_ROUND_START_URI, SECOND_ROUND_GO_URI, SECOND_ROUND_LAUGH_URI, SECOND_ROUND_NO_LAUGH_URI, SECOND_ROUND_START_URI, ERROR_URI, RESULT_URI, STATUS_URI } = DESTINATION_URI;
-  const [firstRoundResult, setFirstRoundResult] = useState('')
-  const [secondRoundResult, setSecondRoundResult] = useState('')
+  const {
+    FIRST_ROUND_GO_URI,
+    FIRST_ROUND_NO_LAUGH_URI,
+    FIRST_ROUND_LAUGH_URI,
+    FIRST_ROUND_START_URI,
+    SECOND_ROUND_GO_URI,
+    SECOND_ROUND_LAUGH_URI,
+    SECOND_ROUND_NO_LAUGH_URI,
+    SECOND_ROUND_START_URI,
+    ERROR_URI,
+    RESULT_URI,
+    STATUS_URI,
+  } = DESTINATION_URI;
+  const [firstRoundResult, setFirstRoundResult] = useState('');
+  const [secondRoundResult, setSecondRoundResult] = useState('');
 
-  const subscribeGame = () =>{
+  const subscribeGame = () => {
     // 1라운드 관련 구독
-    client?.subscribe(FIRST_ROUND_GO_URI, (message)=>{
-      console.log('1라운드 메세지: ', message)
+    client?.subscribe(FIRST_ROUND_GO_URI, (message) => {
+      console.log('1라운드 메세지: ', message);
       if (message.body == 'START') {
-        console.log('1라운드 시작')
+        console.log('1라운드 시작');
       }
-    })
-    client?.subscribe(FIRST_ROUND_NO_LAUGH_URI, (message)=>{
-      console.log('1라운드 메세지: ', message)
-      setFirstRoundResult(message.body)
-    })
-    client?.subscribe(FIRST_ROUND_LAUGH_URI, (message)=>{
-      console.log('1라운드 메세지: ', message)
-      setFirstRoundResult(message.body)
-    })
+    });
+    client?.subscribe(FIRST_ROUND_NO_LAUGH_URI, (message) => {
+      console.log('1라운드 메세지: ', message);
+      setFirstRoundResult(message.body);
+    });
+    client?.subscribe(FIRST_ROUND_LAUGH_URI, (message) => {
+      console.log('1라운드 메세지: ', message);
+      setFirstRoundResult(message.body);
+    });
 
     // 2라운드 관련 구독
-    client?.subscribe(SECOND_ROUND_GO_URI, (message)=>{
-      console.log('2라운드 메세지: ', message)
+    client?.subscribe(SECOND_ROUND_GO_URI, (message) => {
+      console.log('2라운드 메세지: ', message);
       if (message.body == 'START') {
-        console.log('2라운드 시작')
+        console.log('2라운드 시작');
       }
-    })
-    client?.subscribe(SECOND_ROUND_NO_LAUGH_URI, (message)=>{
-      console.log('2라운드 메세지: ', message)
-      setSecondRoundResult(message.body)
-    })
-    client?.subscribe(SECOND_ROUND_LAUGH_URI, (message)=>{
-      console.log('2라운드 메세지: ', message)
-      setSecondRoundResult(message.body)
-    })
+    });
+    client?.subscribe(SECOND_ROUND_NO_LAUGH_URI, (message) => {
+      console.log('2라운드 메세지: ', message);
+      setSecondRoundResult(message.body);
+    });
+    client?.subscribe(SECOND_ROUND_LAUGH_URI, (message) => {
+      console.log('2라운드 메세지: ', message);
+      setSecondRoundResult(message.body);
+    });
 
     // error
-    client?.subscribe(ERROR_URI, (message)=>{
-      console.log('error: ', message)
-    })
-  }
-  return <div>스톰프야 일해줘</div>;
+    client?.subscribe(ERROR_URI, (message) => {
+      console.log('error: ', message);
+    });
+  };
+
+  // 웹소켓 관련
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  useEffect(() => {
+    // 웹캠 연결
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      })
+      .catch((err) => console.log('웹캠 에러:', err));
+    // WebSocket 연결
+    const PYTHON_URL = process.env.NEXT_PUBLIC_PYTHON_URL;
+    const websocket = new WebSocket(`${PYTHON_URL}`);
+    setWs(websocket);
+    websocket.onopen = () => console.log('WebSocket 연결됨');
+    websocket.onmessage = (event) => console.log('서버로부터 메세지 받음:', event.data);
+    websocket.onerror = (error) => console.log('WebSocket 에러:', error);
+    websocket.onclose = () => console.log('WebSocket 연결 종료됨');
+    // 웹캠 이미지 캡쳐 및 전송
+    const captureAndSend = () => {
+      if (canvasRef.current && videoRef.current) {
+        const ctx = canvasRef.current.getContext('2d');
+        if (ctx) {
+          ctx.drawImage(videoRef.current, 0, 0, 640, 480); // 여기서 640x480은 캡처할 이미지의 크기입니다. 원하는대로 조절하세요.
+          const dataURL = canvasRef.current.toDataURL('image/jpeg');
+          if (websocket.readyState === WebSocket.OPEN) {
+            const message = {
+              image: dataURL,
+              header: {
+                round: 1,
+                gameSessionId: '12345',
+              },
+            };
+            websocket.send(JSON.stringify(message));
+          }
+        }
+      }
+    };
+    const intervalId = setInterval(captureAndSend, 1000);
+    return () => {
+      clearInterval(intervalId);
+      if (websocket) {
+        websocket.close();
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <video ref={videoRef} width="640" height="480" autoPlay muted />
+      <canvas ref={canvasRef} width="640" height="480" style={{ display: 'none' }} />
+    </div>
+  );
 };
 
 export default PlayPage;
