29bf27f861a15bf2d44ef78dfb697363c97ac777
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-06 17:43:26 +0900

    fix: controller, service 일 분리(전영호 코드리뷰 반영)

--- backend/dgrr/src/main/java/live/dgrr/domain/kakao/controller/KakaoController.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/kakao/controller/KakaoController.java
@@ -1,13 +1,9 @@
 package live.dgrr.domain.kakao.controller;
 
-import jakarta.servlet.http.HttpSession;
 import live.dgrr.domain.kakao.dto.response.KakaoLoginResponse;
 import live.dgrr.domain.kakao.service.KakaoService;
 import live.dgrr.domain.member.dto.response.MemberLoginResponse;
 import live.dgrr.domain.member.dto.response.MemberResponse;
-import live.dgrr.domain.ranking.service.RankingService;
-import live.dgrr.global.security.jwt.JwtProperties;
-import live.dgrr.global.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/kakao")
 public class KakaoController {
     private final KakaoService kakaoService;
-    private final TokenProvider tokenProvider;
 
     // 카카오 로그인
     @GetMapping("/kakaoCallback")
@@ -28,7 +23,6 @@ public class KakaoController {
         KakaoLoginResponse response;
         String id = kakaoService.createKakaoUser(token);
         MemberResponse member = kakaoService.getMemberByKakaoId(id);
-        System.out.println("member: " + member);
         if(member == null) { // 멤버가 없으면 회원가입
             response = KakaoLoginResponse.of("signUp", id, null);
         }else { // 멤버가 있다면 로그인
@@ -40,7 +34,7 @@ public class KakaoController {
     @GetMapping("/login/{kakaoId}")
     public ResponseEntity<MemberLoginResponse> login(@PathVariable(value="kakaoId") String kakaoId) {
         MemberResponse member = kakaoService.getMemberByKakaoId(kakaoId);
-        MemberLoginResponse response = new MemberLoginResponse(JwtProperties.TOKEN_PREFIX+tokenProvider.generateTokenDto(member.kakaoId(), member.memberId()).accessToken(), member);
+        MemberLoginResponse response = kakaoService.getTokenFromMember(member);
         return new ResponseEntity(response, HttpStatus.OK);
     }
 
