4a15212faea8b44fe2f3da4631c766415223f2f5
전영호 <aplblypt@gmail.com>
2023-11-06 14:13:15 +0900

    chore: 안쓰는 import 문 삭제

--- backend/dgrr/src/main/java/live/dgrr/domain/game/dto/GameResultResponse.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/game/dto/GameResultResponse.java
@@ -2,9 +2,6 @@ package live.dgrr.domain.game.dto;
 
 import live.dgrr.domain.game.entity.GameMember;
 import live.dgrr.domain.game.entity.GameResult;
-import live.dgrr.domain.game.entity.event.GameType;
-import live.dgrr.domain.gamehistory.entity.GameHistory;
-import live.dgrr.domain.member.entity.Member;
 import live.dgrr.global.entity.Tier;
 import lombok.Builder;
 
