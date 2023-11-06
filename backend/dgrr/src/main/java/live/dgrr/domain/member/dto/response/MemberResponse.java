ddb60e97203ec635eda0698ae139916c98b43965
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-06 16:57:36 +0900

    feat: MemberResponse에 kakaoId 추가

--- backend/dgrr/src/main/java/live/dgrr/domain/member/dto/response/MemberResponse.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/member/dto/response/MemberResponse.java
@@ -1,19 +1,20 @@
 package live.dgrr.domain.member.dto.response;
 
-import live.dgrr.domain.member.dto.request.MemberAddRequest;
 import live.dgrr.domain.member.entity.Member;
 import lombok.Builder;
 
 @Builder
 public record MemberResponse(
         Long memberId,
+        String kakaoId,
         String nickname,
         String profileImage,
         String description
 ) {
-    public static MemberResponse of(Long memberId, String nickname, String profileImage, String description) {
+    public static MemberResponse of(Long memberId, String kakaoId, String nickname, String profileImage, String description) {
         return MemberResponse.builder()
                 .memberId(memberId)
+                .kakaoId(kakaoId)
                 .nickname(nickname)
                 .profileImage(profileImage)
                 .description(description)
@@ -23,6 +24,7 @@ public record MemberResponse(
     public static MemberResponse of(Member member) {
         return MemberResponse.builder()
                 .memberId(member.getMemberId())
+                .kakaoId(member.getKakaoId())
                 .nickname(member.getNickname())
                 .profileImage(member.getProfileImage())
                 .description(member.getDescription())
