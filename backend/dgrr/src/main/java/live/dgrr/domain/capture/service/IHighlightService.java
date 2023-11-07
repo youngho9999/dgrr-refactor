46baf9badd70f005bb844e5eae7e742b7914a82d
itsdangerous <danger121@naver.com>
2023-11-07 11:08:26 +0900

    feat: HighlightService Interface 추가

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/service/IHighlightService.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/service/IHighlightService.java
@@ -2,7 +2,7 @@ package live.dgrr.domain.capture.service;
 
 import live.dgrr.domain.capture.entity.HighlightID;
 
-public interface HighlightService {
+public interface IHighlightService {
     void saveHighlight(HighlightID highlightID, String encodedImage);
 
     String highlightImage(String gameRoomId, int round);
