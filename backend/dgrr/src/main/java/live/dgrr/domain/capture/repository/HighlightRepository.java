d12f72c090a7d12a7b5aa6e9faa0cdb3dc1e026d
itsdangerous <danger121@naver.com>
2023-11-07 11:04:52 +0900

    feat: Highlight Entity 관리하는 리포지토리 추가

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/repository/HighlightRepository.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/repository/HighlightRepository.java
@@ -1,2 +1,9 @@
-package live.dgrr.domain.capture.repository;public interface CaptrueREpository {
+package live.dgrr.domain.capture.repository;
+
+import live.dgrr.domain.capture.entity.Highlight;
+import live.dgrr.domain.capture.entity.HighlightID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HighlightRepository extends JpaRepository<Highlight, HighlightID> {
+
 }
