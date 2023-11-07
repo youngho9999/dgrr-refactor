4ccdcbbd853976436651df73dadcc6815fedc6f7
itsdangerous <danger121@naver.com>
2023-11-07 11:02:42 +0900

    feat: Highlight 객체 생성

--- backend/dgrr/src/main/java/live/dgrr/domain/capture/entity/Highlight.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/capture/entity/Highlight.java
@@ -1,2 +1,25 @@
-package live.dgrr.domain.capture.entity;public class Highlight {
+package live.dgrr.domain.capture.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.io.Serializable;
+import java.net.URL;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "highlight")
+public class Highlight implements Serializable {
+
+    @EmbeddedId
+    HighlightID highlightID;
+
+    @Column(name = "capture_url")
+    URL captureUrl;
 }
