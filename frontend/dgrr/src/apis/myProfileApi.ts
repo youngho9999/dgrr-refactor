3d45540620db034d207eef7e19714dfe9b94e17a
337ccc <337ccc@gmail.com>
2023-11-08 18:05:38 +0900

    feat: 회원 정보 가져오는 API 로직 작성 및 연결

---
+++ frontend/dgrr/src/apis/myProfileApi.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { setUrl } from '@/utils/setUrl';
+
+const apiUrl = `http://localhost:8080/api/v1/member`;
+// const apiUrl = `${setUrl}/member`;
+
+export interface GameHistoryProps {
+  gameHistoryId: number;
+  gameRoomId: number;
+  gameResult: string;
+  gameType: string;
+  gameTime: number;
+  holdingTime: number;
+  ratingChange: number;
+  highlightImage: string;
+  createdAt: string;
+  opponentNickname: string;
+  opponentProfileImage: string;
+  opponentDescription: string;
+}
+
+export interface GetMyInfoProps {
+  member: {
+    memberId: number;
+    nickname: string;
+    profileImage: string;
+    description: string;
+  };
+  ranking: {
+    season: number;
+    score: number;
+    rank: number;
+    tier: string;
+  };
+  gameHistoryList: GameHistoryProps[];
+}
+
+export interface PutMyInfoProps {
+  member: {
+    memberId: number;
+    nickname: string;
+    profileImage: string;
+    description: string;
+  };
+}
+
+export const getMyInfoApi = async () => {
+  // axios.defaults.headers.common["Authorization"] = localStorage.getItem("token");
+  axios.defaults.headers.common["Authorization"] = 1
+  const res = await axios.get<GetMyInfoProps>(`${apiUrl}/member-id`);
+  console.log('회원 정보 확인', res.data)
+  return res.data;
+};
