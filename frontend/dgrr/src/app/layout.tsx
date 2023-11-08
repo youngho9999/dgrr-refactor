454a1bb628d41943d4fccc569ae8f06350b21558
이수연 <sooyeon990828@naver.com>
2023-11-08 14:56:30 +0900

    feat: default 커서 적용

--- frontend/dgrr/src/app/layout.tsx
+++ frontend/dgrr/src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
+      <body className={`${inter.className} cursor-default`}>
         <Providers>{children}</Providers>
       </body>
     </html>
