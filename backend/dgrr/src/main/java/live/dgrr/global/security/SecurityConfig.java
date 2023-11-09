14ede104de54605be790eb91df429f2c6c6f3ce1
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-09 13:54:06 +0900

    feat: SecurityConfig 수정

--- backend/dgrr/src/main/java/live/dgrr/global/security/SecurityConfig.java
+++ backend/dgrr/src/main/java/live/dgrr/global/security/SecurityConfig.java
@@ -1,63 +1,36 @@
 package live.dgrr.global.security;
 
-import live.dgrr.global.config.url.UrlConfig;
-import live.dgrr.global.security.jwt.JwtAccessDeniedHandler;
-import live.dgrr.global.security.jwt.JwtAuthenticationEntryPoint;
-import live.dgrr.global.security.jwt.JwtAuthenticationFilter;
-import live.dgrr.global.security.jwt.TokenProvider;
+import live.dgrr.global.security.jwt.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final String FRONT_URL;
-
-    public SecurityConfig(UrlConfig urlConfig) {
-        this.FRONT_URL = urlConfig.getFront();
-    }
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final TokenProvider tokenProvider;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter, CorsConfigurationSource corsConfigurationSource) throws Exception {
-        System.out.println(http.toString());
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .formLogin(fl -> fl.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource))
-                .exceptionHandling(eh -> eh.authenticationEntryPoint(new JwtAuthenticationEntryPoint()).accessDeniedHandler(new JwtAccessDeniedHandler()))
+                .cors(cors -> cors.disable())
+                .exceptionHandling(eh -> eh.authenticationEntryPoint(jwtAuthenticationEntryPoint).accessDeniedHandler(jwtAccessDeniedHandler))
                 .authorizeRequests((requests) -> requests
-                        .requestMatchers("/api/v1/kakao/**", "/ws", "/api/v1/member", "/api/v1/member/", "/api/v1/member/**").permitAll()
+                        .requestMatchers("/api/v1/kakao/**", "/ws", "/api/v1/member", "/api/v1/member/").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .apply(new JwtSecurityConfig(tokenProvider));
         return http.build();
     }
 
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(TokenProvider tokenProvider) {
-        return new JwtAuthenticationFilter(tokenProvider);
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(FRONT_URL)); // 허용할 Origin 지정
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
