fa00a1cfdc79b6c6a9965f043bdf932dd56e153b
mjpark1329@naver.com <mjpark1329@naver.com>
2023-11-10 09:32:45 +0900

    fix: 커스텀 cors configuration 적용

--- backend/dgrr/src/main/java/live/dgrr/global/security/SecurityConfig.java
+++ backend/dgrr/src/main/java/live/dgrr/global/security/SecurityConfig.java
@@ -1,29 +1,43 @@
 package live.dgrr.global.security;
 
+import live.dgrr.global.config.jwt.JwtConfig;
 import live.dgrr.global.security.jwt.*;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final TokenProvider tokenProvider;
+    private final String FRONT_URL;
+
+    public SecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler, TokenProvider tokenProvider, JwtConfig jwtConfig) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.tokenProvider = tokenProvider;
+        this.FRONT_URL = jwtConfig.getSecret();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        System.out.println("FRONT: " + FRONT_URL);
         http
                 .formLogin(fl -> fl.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .exceptionHandling(eh -> eh.authenticationEntryPoint(jwtAuthenticationEntryPoint).accessDeniedHandler(jwtAccessDeniedHandler))
                 .authorizeRequests((requests) -> requests
                         .requestMatchers("/api/v1/kakao/**", "/api/connect", "/file/**", "/api/v1/member", "/api/v1/member/").permitAll()
@@ -33,4 +47,17 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        System.out.println("FRONT: " + FRONT_URL);
+        configuration.setAllowedOrigins(Arrays.asList("*")); // 허용할 Origin 지정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
