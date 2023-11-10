a46e62873a7963c7d726f01bd246bad0da84fb58
itsdangerous <danger121@naver.com>
2023-11-10 17:54:07 +0900

    design: 배치 수정 및 애니메이션 추가

--- frontend/dgrr/src/app/game/match/page.tsx
+++ frontend/dgrr/src/app/game/match/page.tsx
@@ -1,10 +1,16 @@
 'use client';
+import opositProfileImage from '@/../public/images/m-bread1.png';
+import myProfileImage from '@/../public/images/m-bread2.png';
+import matchAttackImage from '@/../public/images/match-attack.png';
+import vsImage from '@/../public/images/vsI-image.png';
 import { useAppSelector } from '@/store/hooks';
+import { Image } from 'next/dist/client/image-component';
 import { useRouter } from 'next/navigation';
 import { Device, OpenVidu, Publisher, Session, Subscriber } from 'openvidu-browser';
 import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import './match.scss';
+// import matchDefenseImage from '@/../public/images/match-defense.png';
 
 const MatchPage = () => {
   const [OV, setOV] = useState<OpenVidu>();
@@ -16,6 +22,7 @@ const MatchPage = () => {
   const gameInfo = useAppSelector((state) => state.game.gameInfo);
   const dispatch = useDispatch();
   const router = useRouter();
+  const roleMessage = '상대방을 웃기세요!';
 
   // // 오픈비두 연결
   // const connectOV = () => {
@@ -61,23 +68,70 @@ const MatchPage = () => {
 
   return (
     <>
-      <div className='bg-main-blue w-screen h-screen max-w-[500px]'>
+      <div className='w-screen h-screen min-w-[300px] max-w-[500px] z-0'>
         <div className='flex flex-col justify-between h-full'>
           {/* 내 정보 */}
           <div className='flex flex-col flex-1 justify-end bg-match-white mx-6 my-0'>
-            <div className='flex flex-2 items-center justify-center bg-match-versus py-6 mx-3 my-4 rounded-lg'></div>
-            <div className='flex flex-1 items-end justify-center bg-match-versus py-1 px-3 mx-3 my-2 rounded-lg'>
-              <div className='MatchedPersonBackground1 min-h-[150px]'></div>
+            <div className='flex flex-1 items-center justify-center bg-match-versus py-1 mx-3 m-2 rounded-lg'>
+              <Image
+                className='MatchedPersonProfile w-[20%] rounded-full'
+                alt='역할 이미지'
+                src={matchAttackImage}
+              />
+            </div>
+            <div className='flex flex-2 items-center justify-center bg-match-versus py-6 mx-3 my-0 rounded-lg'>
+              <div className='text-[30px] text-[#fccb5c] font-black'>
+                <p>{roleMessage}</p>
+              </div>
+            </div>
+            <div className='MatchedPerson1 flex flex-1 relative items-end justify-center bg-match-versus py-5 px-3 mx-3 my-2 rounded-lg'>
+              <div className='MatchedPersonBackground1 min-h-[150px] z-40'>
+                <div className='absolute text-[10px] text-[#9cd4ab] mt-4 ml-[80%] mb-[50px]'>
+                  NickName
+                </div>
+
+                <div className='w-[240px] ml-[160px] mt-7 ml-4 text-[#fee691] text-[18px] rounded-lg border-b-2 '>
+                  <p>가나다라마바사아자차카타</p>
+                </div>
+                <div className='absolute text-[10px] text-[#9cd4ab] mt-4 ml-[80%] mb-[50px]'>
+                  상태메시지
+                </div>
+                <div className='ml-[160px] min-w-[150px] max-w-[220px] text-[14px] text-[#f2f2f2] pl-2 mt-6 ml-4 rounded-lg border-b-2 '>
+                  <p>내가 보여준다고 했지??</p>
+                </div>
+              </div>
+
+              <Image
+                className='MatchedPersonProfile w-[160px] mb-[2%] mr-[60%] absolute rounded-full border-0 border-cyan-500 z-50'
+                alt='프로필 이미지'
+                src={myProfileImage}
+              />
             </div>
           </div>
-
+          <div className='VersusImage flex flex-1 absolute ml-[190px] mt-[300px] w-[100px] h-[200px] z-30'>
+            <Image alt='vs이미지' src={vsImage} />
+          </div>
           {/* ------------------------------------------------------------------------------------------------------------- */}
-
           {/* 상대 정보 */}
           <div className='flex flex-col flex-1 justify-end bg-match-white mx-6 my-0'>
-            <div className='flex flex-1 items-start justify-center bg-match-versus py-1 px-3 mx-3 my-2 rounded-lg'>
-              <div className='MatchedPersonBackground2 min-h-[150px]'></div>
+            <div className='MatchedPerson2 flex flex-1 relative items-start justify-center bg-match-versus py-5 px-3 mx-3 my-2 rounded-lg'>
+              <div className='MatchedPersonBackground2 min-h-[150px] z-40'>
+                <div className='absolute text-[10px] text-[#9cd4ab] mt-4 ml-5'>NickName</div>
+                <div className='w-[240px] pl-2 mt-7 ml-4 text-[#fee691] text-[18px] rounded-lg border-b-2 '>
+                  <p>가나다라마바사아자차카타</p>
+                </div>
+                <div className='absolute text-[10px] text-[#9cd4ab] mt-3 ml-5'>상태메시지</div>
+                <div className='min-w-[150px] max-w-[220px] text-[14px] text-[#f2f2f2] pl-2 mt-6 ml-4 rounded-lg border-b-2 '>
+                  <p>내가 보여준다고 했지??</p>
+                </div>
+              </div>
+              <Image
+                className='MatchedPersonProfile w-[160px] mt-[2%] ml-[52%] absolute rounded-full border-0 border-cyan-500 z-50'
+                alt='프로필 이미지'
+                src={opositProfileImage}
+              />
             </div>
+
             <div className='flex flex-2 items-center justify-center bg-match-versus py-6 mx-3 my-4 rounded-lg'></div>
           </div>
         </div>
