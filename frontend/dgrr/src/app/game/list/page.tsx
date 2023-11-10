f2a9d8c77171163cf45cf36dfd83120330c3e40b
hyuna <kook980316@gmail.com>
2023-11-10 14:45:51 +0900

    fix: 게임 선택 창에서 선택시마다 client 생성하도록 변경

--- frontend/dgrr/src/app/game/list/page.tsx
+++ frontend/dgrr/src/app/game/list/page.tsx
@@ -10,10 +10,12 @@ import { useEffect, useState } from 'react';
 import { Client, StompHeaders } from '@stomp/stompjs';
 import { useDispatch } from 'react-redux';
 import { createClient } from '@/store/gameSlice';
+import axios from 'axios';
+import { saveRoomCode } from '@/store/roomSlice';
 
 const ListPage = () => {
   const gameList = [
-    { value: '방 만들기', imgLink: doorImg, navLink: '/main' },
+    { value: '방 만들기', imgLink: doorImg, navLink: '/room/loading' },
     { value: '방 찾기', imgLink: keyImg, navLink: '' },
     { value: '랜덤 매칭', imgLink: welcomeImg, navLink: '/game/loading' },
   ];
@@ -24,7 +26,8 @@ const ListPage = () => {
   };
   const [isModal, setIsModal] = useState(false);
   const dispatch = useDispatch();
-  const [memberId, setMemberId] = useState('')
+  const [memberId, setMemberId] = useState('');
+  const backUrl = process.env.NEXT_PUBLIC_BACK_URL;
 
   const connectStomp = (headers: StompHeaders) => {
     const client = new Client({
@@ -48,34 +51,79 @@ const ListPage = () => {
     };
   };
 
-  useEffect(() => {
-    const memberId = localStorage.getItem('memberId')
-    if (memberId) {
-      setMemberId(memberId)
+  const makeRoom = (headers: StompHeaders) => {
+    const client = new Client({
+      brokerURL: process.env.NEXT_PUBLIC_BROKER_URL,
+      connectHeaders: {
+        ...headers,
+      },
+      debug: (message) => {
+        console.log('[Stomp Debug :: message]', message); // 웹소켓 디버깅 로그 추가
+      },
+    });
+
+    // 클라이언트 활성화
+    client.activate();
+
+    client.onConnect = (frame) => {
+      console.log('연결');
+      console.log(client);
+      // redux에 client 저장
+      dispatch(createClient(client));
+    };
+
+    if (client) {
+      axios
+        .post(`${backUrl}/waiting-room`)
+        .then((res) => {
+          // console.log(res.data);
+          if (res.data) {
+            const roomCode = res.data;
+            dispatch(saveRoomCode(roomCode));
+          }
+        })
+        .catch((err) => {
+          console.log('내용: ', err.message);
+        });
     }
-  }, [])
+  };
+
+  const joinRoom = () => {
+    setIsModal(!isModal);
+    connectStomp({ Authorization: memberId });
+  };
+  useEffect(() => {
+    const memberId = localStorage.getItem('memberId');
+    if (memberId) {
+      setMemberId(memberId);
+    }
+  }, []);
 
   return (
-    <div className="bg-main-blue w-screen h-screen max-w-[500px] min-h-[565px]">
-      <Header headerType="GAMESTART" />
+    <div className='bg-main-blue w-screen h-screen max-w-[500px] min-h-[565px]'>
+      <Header headerType='GAMESTART' />
       {isModal && <FindRoomModal handleModal={handleModal} />}
 
-      <div className="pt-6 space-y-6">
+      <div className='pt-6 space-y-6'>
         {gameList.map((item, index) =>
           index === 1 ? (
-            <div className={`${commonClass} cursor-hover`} key={index} onClick={handleModal}>
-              <Image src={item.imgLink} alt="이미지예시" className="w-20 h-20" />
-              <p className="font-bold text-base">{item.value}</p>
+            <div className={`${commonClass} cursor-hover`} key={index} onClick={joinRoom}>
+              <Image src={item.imgLink} alt='이미지예시' className='w-20 h-20' />
+              <p className='font-bold text-base'>{item.value}</p>
             </div>
           ) : (
             <button
               key={index}
-              onClick={() => connectStomp({ Authorization: memberId })}
+              onClick={
+                index === 0
+                  ? () => makeRoom({ Authorization: memberId })
+                  : () => connectStomp({ Authorization: memberId })
+              }
               className={commonClass}
             >
-              <Link href={item.navLink} className="w-full h-full flex flex-col items-center">
-                <Image src={item.imgLink} alt="이미지예시" className="w-20 h-20" />
-                <p className="font-bold text-base">{item.value}</p>
+              <Link href={item.navLink} className='w-full h-full flex flex-col items-center'>
+                <Image src={item.imgLink} alt='이미지예시' className='w-20 h-20' />
+                <p className='font-bold text-base'>{item.value}</p>
               </Link>
             </button>
           )
