f740602c2d4f3a1c36ff07d764388603e2d8e994
이수연 <sooyeon990828@naver.com>
2023-11-10 15:59:11 +0900

    feat: LinkButton 클릭이벤트 추가

--- frontend/dgrr/src/components/LinkButton.tsx
+++ frontend/dgrr/src/components/LinkButton.tsx
@@ -4,14 +4,16 @@ type ButtonProps = {
   value: string;
   small?: boolean;
   moveLink: string;
+  clickEvent?: () => void;
 };
 
-export const LinkButton = ({ value, small = false, moveLink }: ButtonProps) => {
+export const LinkButton = ({ value, small = false, moveLink, clickEvent}: ButtonProps) => {
   const size = small ? 'w-20' : 'w-4/5';
   return (
     <Link
       href={moveLink}
       className={`text-center text-base font-bold uppercase leading-none text-zinc-800 max-w-xs ${size} cursor-hover`}
+      onClick={clickEvent}
     >
       <div
         className={`shadow-[inset_0_-2px_4px] shadow-[#FFA73F] rounded-lg border-2 py-5 hover:brightness-110 bg-gradient-to-b from-start-yellow to-end-yellow border-zinc-800`}
