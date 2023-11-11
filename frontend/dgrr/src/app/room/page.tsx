b12c61ae31b6d14f5019e417051338d2ca6533d7
hyuna <kook980316@gmail.com>
2023-11-12 02:04:09 +0900

    feat: 대기방에서 본인 화면 추가

--- frontend/dgrr/src/app/room/page.tsx
+++ frontend/dgrr/src/app/room/page.tsx
@@ -9,8 +9,10 @@ import { setReady } from '@/store/roomSlice';
 import { stompConfig } from '@/types/game';
 import { roomStompConfig } from '@/types/room';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import character from '@/../public/images/logo_character.png';
+import Image from 'next/image';
 
 const RoomPage = () => {
   const client = useAppSelector((state) => state.game.client);
@@ -25,7 +27,8 @@ const RoomPage = () => {
   const { DESTINATION_URI } = stompConfig;
   const { GAME_URI } = DESTINATION_URI;
   const value = readyState ? '취소하기' : '준비하기';
-
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef(null);
   const subscribeRoom = () => {
     client?.subscribe(READY_SUB_URI, (message) => {
       const content = JSON.parse(message.body);
@@ -55,6 +58,7 @@ const RoomPage = () => {
       publishMessage(client, READY_SEND_URI, '');
     }
   };
+
   useEffect(() => {
     if (client) {
       subscribeRoom();
@@ -63,25 +67,42 @@ const RoomPage = () => {
     if (memberId) {
       setMemberId(memberId);
     }
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      })
+      .catch((err) => console.log('웹캠 에러:', err));
   }, [client, memberId]);
 
   return (
-    <div className='w-screen h-screen max-w-[500px] min-h-[565px] bg-black'>
+    <div className='w-screen h-screen max-w-[500px] min-h-[565px] bg-black relative'>
       <Header headerType='WAITING' roomCode={roomcode} />
-      {owner !== memberId ? (
-        <FuncButton value={value} small={true} clickEvent={readyOnOff} />
-      ) : readyState ? (
-        <FuncButton value='시작하기' small={true} clickEvent={startGame} />
-      ) : (
-        <button
-          disabled
-          className={`rounded-lg border-2 max-w-xs py-5 hover:brightness-110 w-20 cursor-hover bg-slate-300`}
-        >
-          <p className={`text-zinc-800 text-center text-base font-bold uppercase leading-none`}>
-            시작하기
-          </p>
-        </button>
-      )}
+      <div className='userVideo mx-3'>
+        <video ref={videoRef} className='object-none h-full' autoPlay muted />
+        <canvas ref={canvasRef} width='640' height='480' style={{ display: 'none' }} />
+      </div>
+      <div className='absolute left-1/2 -ml-8'>
+        {owner !== memberId ? (
+          <FuncButton value={value} small={true} clickEvent={readyOnOff} />
+        ) : readyState ? (
+          <FuncButton value='시작하기' small={true} clickEvent={startGame} />
+        ) : (
+          <button
+            disabled
+            className={`rounded-lg border-2 max-w-xs py-5 hover:brightness-110 w-20 cursor-hover bg-slate-300`}
+          >
+            <p className={`text-zinc-800 text-center text-base font-bold uppercase leading-none`}>
+              시작하기
+            </p>
+          </button>
+        )}
+      </div>
+      <div className='userVideo bg-white grid place-items-center mt-3 mx-3'>
+        <Image src={character} alt='식빵' className='w-40 h-40' />
+      </div>
     </div>
   );
 };
