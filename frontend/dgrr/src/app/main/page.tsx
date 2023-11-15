4130ec2ee307dd345fe5cfb04dc8e205690bf6df
337ccc <337ccc@gmail.com>
2023-11-15 10:31:56 +0900

    design: 메인 페이지 위치 조정

--- frontend/dgrr/src/app/main/page.tsx
+++ frontend/dgrr/src/app/main/page.tsx
@@ -57,35 +57,38 @@ const MainPage = () => {
     requestCameraPermission();
   }, []);
   return (
-    <div className="bg-main-blue w-screen h-screen min-h-[580px] max-w-[500px]">
-      <Header headerType="MAIN" />
+    <div className='bg-main-blue w-screen h-screen min-h-[580px] max-w-[500px]'>
+      <Header headerType='MAIN' />
       {openModal && <ExplainModal onClose={handleModal} />}
       {openTutorialModal && <TutorialModal onClose={handleTutorialModal} />}
 
-      <div className="flex flex-col h-5/6 pt-10">
+      <div className='flex flex-col h-5/6 pt-12'>
         <div>
           {/* 식빵이 이미지 */}
-          <div className="flex justify-center mb-5">
-            <Image alt="캐릭터" src={character} className="w-40 h-40 hover:animate-spin" />
+          <div className='flex justify-center mb-6'>
+            <Image alt='캐릭터' src={character} className='w-40 h-40 hover:animate-spin' />
           </div>
           {/* 데구르르 로고 */}
-          <div className="flex justify-center 2sm:px-20 px-14">
-            <Image alt="타이틀" src={title} className="ms-3 z-10" />
+          <div className='flex justify-center 2sm:px-20 px-14'>
+            <Image alt='타이틀' src={title} className='ms-3 z-10' />
           </div>
         </div>
-        <div className="space-y-6 mt-10">
+        <div className='space-y-9 3sm:mt-12 mt-14'>
           {/* 튜토리얼 버튼 */}
           {/* 버튼에 handleTutorialModal 추가하면 댐*/}
-          <div className="flex justify-center animate-slideDown -z-50" onClick={handleTutorialModal}>
-            <div className="h-20 w-20 -top-50 -left-50">
-              <img src={clickMe} alt="click me" />
+          <div
+            className='flex justify-center animate-slideDown -z-50'
+            onClick={handleTutorialModal}
+          >
+            <div className='h-20 w-20 -top-50 -left-50'>
+              <img src={clickMe} alt='click me' />
             </div>
-            <img src={logoTutoral} alt="튜토리얼" />
+            <img src={logoTutoral} alt='튜토리얼' />
           </div>
           {/* 게임 시작 버튼 */}
-          <div className="flex justify-center">
-            <div className="w-4/5 2sm:w-[320px]">
-              <LinkButton value="게임 시작" moveLink="/game/list" clickEvent={playSound} />
+          <div className='flex justify-center'>
+            <div className='w-4/5 2sm:w-[320px]'>
+              <LinkButton value='게임 시작' moveLink='/game/list' clickEvent={playSound} />
             </div>
           </div>
         </div>
