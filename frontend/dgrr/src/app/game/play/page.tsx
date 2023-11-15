6ebcec7780b59c38de16fda241c0783dd9bb8647
hyuna <kook980316@gmail.com>
2023-11-15 10:29:43 +0900

    fix: 캡쳐 화면 이미지 크기 조정

--- frontend/dgrr/src/app/game/play/page.tsx
+++ frontend/dgrr/src/app/game/play/page.tsx
@@ -212,6 +212,8 @@ const PlayPage = () => {
     if (publisher && canvasRef.current) {
       const videoElement = publisher.videos[0].video;
       const canvas = canvasRef.current;
+      canvas.width = videoElement.videoWidth;
+      canvas.height = videoElement.videoHeight;
       const ctx = canvas.getContext('2d');
 
       // Canvas에 비디오 화면 그리기
@@ -279,7 +281,7 @@ const PlayPage = () => {
     <div className='w-screen h-screen max-w-[500px] min-h-[565px] bg-black'>
       <Header headerType='GAME' />
       {modalOpen && <GameStateModal when={when} gameState={turn} />}
-      <div className='relative userVideo'>
+      <div className='relative'>
         <Image
           src={
             round === 'first'
@@ -301,7 +303,7 @@ const PlayPage = () => {
           className={`w-4 h-4 rounded-full ml-3 ${recognition ? 'bg-green-500' : 'bg-red-600'}`}
         ></div>
       </div>
-      <div className='relative userVideo'>
+      <div className='relative'>
         {!modalOpen && (
           <Image
             src={
