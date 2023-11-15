4765f06f87e9b1944745de92e1bc528cb3c766b6
337ccc <337ccc@gmail.com>
2023-11-15 15:00:25 +0900

    fix: 방 로딩 페이지에서 튜토리얼 제거

--- frontend/dgrr/src/app/room/loading/page.tsx
+++ frontend/dgrr/src/app/room/loading/page.tsx
@@ -5,14 +5,11 @@ import { useAppSelector } from '@/store/hooks';
 import { saveRoomInfo } from '@/store/roomSlice';
 import { roomStompConfig } from '@/types/room';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import character from '@/../../public/images/floating_bread_cropped.gif';
 import Image from 'next/image';
 import Toast from '@/components/elements/Toast';
-import ButtonClickAudio from '@/components/audio/ButtonClickAudio';
-import { ExplainModal } from '@/components/elements/ExplainModal';
-import { TutorialModal } from '@/components/elements/TutorialModal';
 
 const RommLoadingPage = () => {
   const client = useAppSelector((state) => state.game.client);
@@ -21,21 +18,6 @@ const RommLoadingPage = () => {
   const dispatch = useDispatch();
   const router = useRouter();
   const roomCode = useAppSelector((state) => state.room.roomCode);
-  const [openModal, setOpenModal] = useState(false);
-  const [openTutorialModal, setOpenTutorialModal] = useState(false);
-  const playSound = ButtonClickAudio();
-  const logoTutoral = '/images/logo_tutorial.png';
-  const clickMe = '/images/click_me.png';
-
-  const handleModal = () => {
-    playSound();
-    setOpenModal(!openModal);
-  };
-
-  const handleTutorialModal = () => {
-    playSound();
-    setOpenTutorialModal(!openTutorialModal);
-  };
 
   const subscribeEnter = () => {
     if (client) {
@@ -69,25 +51,14 @@ const RommLoadingPage = () => {
   return (
     <div className='bg-main-blue w-screen h-screen min-h-[580px] max-w-[500px]'>
       <Header headerType='MATCHING' />
-      {openModal && <ExplainModal onClose={handleModal} />}
-      {openTutorialModal && <TutorialModal onClose={handleTutorialModal} />}
-
-      <div className='flex flex-col justify-between h-3/5 pt-10'>
+      <div className='flex flex-col justify-between pt-12'>
         <div className='flex justify-center mb-10'>
           <Image alt='캐릭터' src={character} />
         </div>
-        <div className='flex justify-center font-bold text-white text-xl mt-4'>
+        <div className='flex justify-center font-bold text-white text-xl'>
           <h1>방 만드는 중</h1>
           <span className='animate-blink'>.</span>
           <span className='animate-blink2'>.</span>.<span className='animate-blink3'>.</span>
-        </div>
-        {/* 튜토리얼 버튼 */}
-        {/* 버튼에 handleTutorialModal 추가하면 댐*/}
-        <div className='flex justify-center animate-slideDown mt-16' onClick={handleTutorialModal}>
-          <div className='h-20 w-20 -top-50 -left-50'>
-            <img src={clickMe} alt='click me' />
-          </div>
-          <img src={logoTutoral} alt='튜토리얼' className='cursor-hover' />
         </div>
       </div>
     </div>
@@ -95,3 +66,4 @@ const RommLoadingPage = () => {
 };
 
 export default RommLoadingPage;
+
